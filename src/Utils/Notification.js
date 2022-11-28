c366281c2514d4ceddb8541f8fb26a576c148906
Saloni Jariwala <salonijariwala.developing@gmail.com>
2022-11-28 09:14:41 +0530

    Add Browser Notification

--- src/Utils/Notification.js
+++ src/Utils/Notification.js
@@ -2,11 +2,19 @@ export const notifyUser = (notificationText = "Notification Enabled") => {
     if (!("Notification" in window)) {
         alert("Browser Not support notification");
     } else if (Notification.permission === "granted") {
-        new Notification(notificationText);
+        const notification = new Notification(notificationText);
+        notification.onclick = (event) => {
+            event.preventDefault(); // prevent the browser from focusing the Notification's tab
+            window.open('http://www.mozilla.org', '_blank');
+        }
     } else if (Notification.permission !== "denied") {
         Notification.requestPermission().then((permission) => {
             if (permission === "granted") {
-                Notification(notificationText);
+                const notification = new Notification(notificationText);
+                notification.onclick = (event) => {
+                    event.preventDefault(); // prevent the browser from focusing the Notification's tab
+                    window.open('http://www.mozilla.org', '_blank');
+                }
             }
         });
     }
